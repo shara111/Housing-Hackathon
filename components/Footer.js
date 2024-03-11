322d672ff04b84206b95882cf938a5f81a38a21a
crishuynh <119613966+CrisH2307@users.noreply.github.com>
2024-03-11 19:31:06 -0400

    OK

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
       <footer className="text-center text-lg-start bg-body-tertiary text-muted" style={{ marginTop: "16vh" }}>
         <section className="d-flex justify-content-center justify-content-lg-between pt-5 pb-4 border-bottom container">
           <div className="me-5 d-none d-lg-block">
-            <p>&copy; 2024 Copyright Sharespace Scout | All Rights Reserved</p>
+            <p>&copy; 2024 Copyright StudentStays | All Rights Reserved</p>
           </div>
           <div>
             <a href="" className="me-4 text-reset"></a>
