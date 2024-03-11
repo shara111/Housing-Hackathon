9f702069c5835fcd120dcee7e23cc79b64b61b3b
Sukhman Hara <shara1@myseneca.ca>
2024-03-11 13:56:14 -0400

    changed homepage

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
       <footer className="text-center text-lg-start bg-body-tertiary text-muted">
         <section className="d-flex justify-content-center justify-content-lg-between pt-4 pb-3 border-bottom container">
           <div className="me-5 d-none d-lg-block">
-            <p>&copy; 2024 Copyright StudentCritic | All Rights Reserved</p>
+            <p>&copy; 2024 Copyright Sharespace Scout | All Rights Reserved</p>
           </div>
           <div>
             <a href="" className="me-4 text-reset"></a>
--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,18 @@ export default function Home() {
   return (
     <div className="container text-center my-5">
       {/* <h1 className="mb-4" style={{ fontSize: '3rem', fontWeight: 'bold' }}>HOMEPAGE</h1> <br /><br /> */}
-      <p className="mb-4" style={{ fontSize: '4rem', fontWeight: 'bold' }}>
+      <p className="mb-4" style={{ fontSize: '4rem', fontWeight: 'bold', marginTop: -'50px'}}>
         Link with current students to <br />secure a place to stay
       </p>
-      <br /><br /><br /><br /><br /><br />
+      <br /><br /><br /><br />
 
       <p className="mb-4" style={{ fontSize: '3rem', fontFamily: '-moz-initial'}}>
         Find a match to hosts willing to welcome you<br /> into their homes, tailored to your needs and <br />preferences
       </p>
-      < br />< br />
+      < br />< br /> <br /> 
 
       <Link href="/profile" passHref>
-        <button className="btn btn-outline-success" style={{ fontSize: '1.75rem'}}>Get Started</button> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /><br /> <br /> <br />
+        <button className="btn btn-outline-success" style={{ fontSize: '1.75rem'}}>Get Started</button> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /><br /> <br /> <br /><br /> <br />
 
       </Link>
     </div>
