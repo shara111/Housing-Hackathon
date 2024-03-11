2a7cce2d7e0e13c84b295d78ff2e67bd48ad08ae
Sukhman Hara <shara1@myseneca.ca>
2024-03-11 12:54:32 -0400

    index.js/homepage

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,23 @@
-import { Inter } from "next/font/google";
 import Link from "next/link";
-
-const inter = Inter({ subsets: ["latin"] });
+import 'bootstrap/dist/css/bootstrap.min.css'; // make sure you have Bootstrap CSS imported
 
 export default function Home() {
   return (
-    <>
-      <h2>Hello</h2>
-      <Link href="/submit">Submit</Link>
-    </>
+    <div className="container text-center my-5">
+      <h1 className="mb-4" style={{ fontSize: '3rem', fontWeight: 'bold' }}>HOMEPAGE</h1> <br /><br />
+      <p className="mb-4" style={{ fontSize: '3rem', fontWeight: 'bold' }}>
+        Link with current homeowners to <br />secure a place to stay
+      </p>
+      <br /><br /><br /><br /><br /><br />
+
+      <p className="mb-4" style={{ fontSize: '4rem', fontFamily: '-moz-initial'}}>
+        Find a match to hosts willing to welcome you<br /> into their homes, tailored to your needs and <br />preferences
+      </p>
+      < br />< br />
+
+      <Link href="/profile" passHref>
+        <button className="btn btn-outline-success" style={{ fontSize: '1.75rem'}}>Get Started</button> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+      </Link>
+    </div>
   );
 }
